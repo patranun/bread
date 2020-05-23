8a4ed8c9cd01cef9b49d26023784ae79e4b47aab
patranun <65813704+patranun@users.noreply.github.com>
2020-05-23 20:01:13 +0700

    Create Exercise4_Patranun_C.py

---
+++ Exercise4_Patranun_C.py
@@ -0,0 +1,15 @@
+x = 80.00
+y = 64.31
+z = 55.55
+q = 92.11
+
+print("--- Your Score ---")
+
+print("Foundation English: ")
+print(x)
+print("General Business:  " )
+print(y)
+print("Introducing to Computer System:" )
+print(y)
+print("Computer Programming:")
+print(q)
