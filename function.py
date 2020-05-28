90810c26c46e1f506b0f9458b11c172968b2072c
patranun <65813704+patranun@users.noreply.github.com>
2020-05-28 14:15:23 +0700

    function

---
+++ function.py
@@ -0,0 +1,16 @@
+x = int(input())
+y = int(input())
+def addnumber(x,y):
+   print(x+y)
+def minus(x,y):
+    print(x-y)
+def multiplier(x,y):
+    print(x*y)
+def division(x,y):
+    print(x/y)
+addnumber(x,y)
+minus(x,y)
+multiplier(x,y)
+division(x,y)
+
+
